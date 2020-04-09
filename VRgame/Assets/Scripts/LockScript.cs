e8dfa72677c0f8623c25edb5aead0a6b2d85101b
Hunter-DeBlase <hunterdeblase@gmail.com>
2020-04-08 20:18:22 -0400

    Added lock and key objects and controllers

---
+++ VRgame/Assets/Scripts/LockScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.CompareTag("Key"))
+        {
+            //Add unlock code here
+
+            //Destroy object last
+            Destroy(col.collider.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
