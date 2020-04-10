6b24126849bb93625cffb3f0c4c385c131addc7c
gjshirey <galenshirey@gmail.com>
2020-04-09 23:47:44 -0400

    Modified LockScript to inlude door rotation variables

--- VRgame/Assets/Scripts/LockScript.cs
+++ VRgame/Assets/Scripts/LockScript.cs
@@ -6,7 +6,13 @@ public class LockScript : MonoBehaviour
 {
 
     public string keyName = "Key";
+    public string doorName = "MasterDoor";
+    public float xangle = -90;
+    public float yangle = 0;
+    public float zangle = 0;
 
+
+    public GameObject Door;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +29,11 @@ public class LockScript : MonoBehaviour
         if (col.collider.CompareTag(keyName))
         {
             //Unlock door
-            
+            //get Door Game Object
+            //Make sure to set the doorName for each door
+            //GameObject[] Door = GameObject.FindGameObjectsWithTag(doorName);
+            Door = GameObject.Find(doorName);
+            Door.transform.Rotate(xangle,yangle,zangle, Space.Self);
             //Destroy object last
             Destroy(col.collider.gameObject);
             Destroy(this.gameObject);
