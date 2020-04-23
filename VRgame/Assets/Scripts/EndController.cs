ed10b0bd30b11d5979421c294fc8405bb76960a7
Hunter-DeBlase <hunterdeblase@gmail.com>
2020-04-23 19:23:52 -0400

    Added end UI and reload scene

---
+++ VRgame/Assets/Scripts/EndController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndController : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.state == GameManager.StateType.END)
+        {
+            endScreen();
+        }
+    }
+
+    public void endScreen()
+    {
+        transform.position = player.transform.position + player.transform.forward/2f;
+        transform.eulerAngles = new Vector3(360, 0, 360) - player.transform.forward;
+        Debug.Log(GameManager.state.ToString());
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- VRgame/Assets/Scripts/LightController.cs
+++ VRgame/Assets/Scripts/LightController.cs
@@ -1,22 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class LightController : MonoBehaviour
 {
     public Light flashlight;
+    public SteamVR_Action_Boolean trigger;
+    public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.RightHand;
     // Start is called before the first frame update
     void Start()
     {
+        trigger.AddOnStateDownListener(TriggerDown, inputSource);
         flashlight = GetComponent<Light>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
-        if (Input.GetKeyDown("f"))
-        {
-            flashlight.enabled = !flashlight.enabled;
-        }
+        flashlight.enabled = !flashlight.enabled;
     }
 }
